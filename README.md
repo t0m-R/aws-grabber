c760c5fcb7e5d4cb580c8987bd6b3da3bdc8c380
Tommaso Rodani <trodani@sissa.it>
2020-02-16 11:32:26 +0100

    0.2.0 works on linux/mac and windows, updated README

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # aws-grabber
-Python Script to grab the AWS Educate credentials in order to  let students use aws-cli
+Python Script to grab the AWS Educate credentials so students can use aws-cli v1.
+
+## About
+This command will update the configuration files of `aws-cli` with new credentials.
+It will grab for you `aws_access_key`, `aws_secret_acces_key` and `aws_session_token` from [Vocareum Labs](https://labs.vocareum.com) and write them in `~/.aws/credentials` and `~/.aws/config`.
+Basic usage:
+```
+python3 grabber.py -u user@mail.com -p passwd
+```
+
+## Installation
+
+Download and extract this repo as a zip file or clone it using HTTPS:
+
+```
+git clone https://github.com/t0m-R/aws-grabber.git
+```
+
+## Dependencies
+This script modifies config files of `aws-cli` and **suppose that you have it already installed**, for more information see [Installing the AWS CLI version 1](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv1.html).
+
+`aws-grabber` use `requests` module for making https requests. You can install it with:
+```
+pip install requests
+```
+
+The other modules are part of the Python Standard Library and are built-in with python.
+
+## Usage
+
+`grabber.py` need username and password to work:
+```
+python3 grabber.py -u user@mail.com -p passwd
+```
+It will update the config files in the default installation path for aws cli with new credentials.
+Existing files will be renamed with a trailing `.old`.
+For more information see [Installing the AWS CLI version 1](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv1.html).
+
+
+
+If you have installed `aws-cli` somewhere else use the flag `--awspath` or `-a` to the location of your `.aws` folder using absolute path notation:
+```
+python3 grabber.py -u user@mail.com -p passwd -a /your/custom/absolute/path
+```
+
+
+
+The default output for `aws-cli` is JSON. You can change it with the flag `--output`, `-o`:
+```
+python3 grabber.py -u user@mail.com -p passwd -o text
+```
+For more information of accepted formats see [How to Select the Output Format](https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-output.html).
+
+
+
+At the moment AWS Students have a `default` profile name and can use only `us-east-1` region.
+If this behaviour changes you can modify these parameters with   `--region`, `-r` flag for region, and `--profile`, `-p` for profile:
+```
+python3 grabber.py -u user@mail.com -p passwd -p MyProfile -r eu-central-1
+```
+For more information of accepted region see [AWS Service Endpoints](https://docs.aws.amazon.com/general/latest/gr/rande.html).
+
+
+## License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
--- grabber.py
+++ grabber.py
@@ -4,18 +4,21 @@ import platform
 import os
 import subprocess
 
-def awspath():
-    if platform.platform().startswith("Windows"):
+def getOS():
+    return os.platform.system()
+
+def awspath(os):
+    if os == "Windows":
         path = "~\\.aws"
     else:
         path = '~/.aws'
     return os.path.expanduser(path)
 
-def arg_parser():
+def arg_parser(os):
     parser = argparse.ArgumentParser(description='Grabber for credentials of AWS Student Pack')
     parser.add_argument('--user', '-u', metavar="user@mail.com", required=True)
     parser.add_argument('--passwd', '-p', metavar='passwd', required=True)
-    parser.add_argument('--awspath', '-a', metavar='.aws/ folder PATH ', default=awspath())
+    parser.add_argument('--awspath', '-a', metavar='.aws/ folder PATH ', default=awspath(os))
     parser.add_argument('--output', '-f', metavar='aws output type', default='json')
     parser.add_argument('--region', '-r', metavar='aws region', default='us-east-1')
     parser.add_argument('--profile', metavar='aws profile name', default='default')
@@ -23,12 +26,13 @@ def arg_parser():
     return args
 
 def main():
-    args = arg_parser()
+    os = getOS()
+    args = arg_parser(os)
     credentials = get_credentials(args.user, args.passwd)
     config = get_config(args.region, args.output)
-    if check_path(args.awspath, 'credentials'):
+    if check_path(os, args.awspath, 'credentials'):
         write(args.awspath, 'credentials', args.profile, credentials)
-    if check_path(args.awspath, 'config'):
+    if check_path(os, args.awspath, 'config'):
         write(args.awspath, 'config', args.profile, config)
 
 def get_config(region, output):
@@ -38,23 +42,32 @@ def get_config(region, output):
     return config
 
 def get_credentials(user, passwd):
+    ua = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
     url  = "https://labs.vocareum.com"
     url2 = url + "/util/vcauth.php"
     payload = { 'sender': 'home', 'loginid': user, 'passwd': passwd}
-    url3 = url + "/util/vcput.php?a=getaws&type=0&stepid=14335&vockey="
+    headers = {'User-Agent':ua, 'Accept-Language': 'en-GB,en;q=0.5'}
+    url3 = url + '/main/main.php?m=course&code=vc_2_0_AWS'
+    url4 = url + '/main/main.php?m=editor&nav=1&asnid=14334&stepid=14335'
+    url5 = url + "/util/vcput.php?a=getaws&nores=0&stepid=14335&mode=s&type=0&vockey="
     with requests.session() as s:
-        s.get(url)
-        s.post(url2,data=payload)
-        url3 += s.cookies['logintoken']
-        aws = s.get(url3)
+        s.get(url, proxies=burp, headers=headers, stream=True)
+        s.post(url2,data=payload, headers=headers, stream=True)
+        s.get(url3, headers=headers, stream=True)
+        s.get(url4, headers=headers, stream=True)
+        url5 += s.cookies['logintoken']
+        aws = s.get(url5, headers=headers, stream=True)
         text = aws.text.split('\n')[1:4]
         return text
 
-def check_path(path, fname):
+def check_path(os, path, fname):
     if os.path.exists(path):
         if os.path.exists(os.path.join(path, fname)):
             print("Found an existing {0} file! Moved to {0}.old".format(fname))
-            subprocess.run(['mv', fname, str(fname+'.old')],cwd=path)
+            if os == "Windows":
+                continue
+            else:
+                subprocess.run(['mv', fname, str(fname+'.old')],cwd=path)
         return True
     else:
         print("Error, path do not exists:")
